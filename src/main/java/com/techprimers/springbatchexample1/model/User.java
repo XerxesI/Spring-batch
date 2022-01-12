21d550a9c23e50d22ed95313d9f639bafb3d20f3
Meelis Kivimäe <meelis.kivimae@gmail.com>
2022-01-12 19:41:11 +0200

    POM.XML Java version was updated to 17
    Readme.md
    H2 selects were added
    
    Lombok related changes added into pom.xml and user.java

--- src/main/java/com/techprimers/springbatchexample1/model/User.java
+++ src/main/java/com/techprimers/springbatchexample1/model/User.java
@@ -1,9 +1,16 @@
 package com.techprimers.springbatchexample1.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class User {
 
@@ -14,65 +21,4 @@ public class User {
     private Integer salary;
     private Date time;
 
-    public User(Integer id, String name, String dept, Integer salary, Date time) {
-        this.id = id;
-        this.name = name;
-        this.dept = dept;
-        this.salary = salary;
-        this.time = time;
-    }
-
-    public User() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDept() {
-        return dept;
-    }
-
-    public void setDept(String dept) {
-        this.dept = dept;
-    }
-
-    public Integer getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Integer salary) {
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuffer sb = new StringBuffer("User{");
-        sb.append("id=").append(id);
-        sb.append(", name='").append(name).append('\'');
-        sb.append(", dept='").append(dept).append('\'');
-        sb.append(", salary=").append(salary);
-        sb.append('}');
-        return sb.toString();
-    }
-
-    public Date getTime() {
-        return time;
-    }
-
-    public void setTime(Date time) {
-        this.time = time;
-    }
 }
